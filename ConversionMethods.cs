fe545d231dcaf9dc48f8bb6cd1b97f93f8d1f697
AstroBurgers <astroburgers1@gmail.com>
2024-05-14 22:33:31 -0500

    Created ConversionMethods.cs

---
+++ ConversionMethods.cs
@@ -0,0 +1,35 @@
+﻿using static CurrencyConverter.CurrencyData;
+
+namespace CurrencyConverter;
+
+public static class ConversionMethods
+{
+    /// <summary>
+    /// Converts the specified amount into the currency specified (eg usd to eur)
+    /// </summary>
+    /// <param name="currencyToConvertTo">What currency to convert to</param>
+    /// <param name="amountToConvert">The amount to convert (e.g $69 - 69)</param>
+    /// <see cref="CurrencyData.Currencies"/>
+    /// <returns>amountToConvert in the specified currency</returns>
+    internal static double ConvertFromUSD(string currencyToConvertTo, double amountToConvert)
+    {
+        double currencyWorth;
+        if (!Currencies.TryGetValue(currencyToConvertTo, out currencyWorth)) return 0;
+        return amountToConvert * currencyWorth;
+    }
+    
+    // Convert to USD method
+    /// <summary>
+    /// Converts the specified amount into usd (eg eur to usd)
+    /// </summary>
+    /// <param name="currencyToConvertFrom">What currency to convert from</param>
+    /// <param name="amountToConvert">The amount to convert (e.g $69 - 69)</param>
+    /// <see cref="CurrencyData.Currencies"/>
+    /// <returns>amountToConvert in usd</returns>
+    internal static double ConvertToUSD(string currencyToConvertFrom, double amountToConvert)
+    {
+        double currencyWorth;
+        if (!Currencies.TryGetValue(currencyToConvertFrom, out currencyWorth)) return 0;
+        return amountToConvert / currencyWorth;
+    }
+}
